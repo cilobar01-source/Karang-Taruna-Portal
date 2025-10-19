043032c8428744e4ac902a376e532c0a8367f42d
cilobar01-source <cilobar01@gmail.com>
2025-10-19 13:24:33 +0700

    Add files via upload

---
+++ components/Providers.js
@@ -0,0 +1,4 @@
+"use client";
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'react-hot-toast';
+export default function Providers({children}){return(<ThemeProvider attribute='class' defaultTheme='dark'><Toaster position='top-center' toastOptions={{duration:2400}}/>{children}</ThemeProvider>)}
