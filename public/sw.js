e57f1c286a4bb42dc2bb74aa2d842daa48999138
cilobar01-source <cilobar01@gmail.com>
2025-10-19 14:46:16 +0700

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",e=>{e.waitUntil(caches.open("karteji-cache-v1").then(c=>c.addAll(["/","/manifest.json","/icons/icon-192x192.png","/icons/icon-512x512.png","/splash/splash.html","/splash/splash-style.css"])))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
